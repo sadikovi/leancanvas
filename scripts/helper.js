645609b72f92750da9da282ace5898a7d56029ee
sadikovi <sadikovi@Ivans-MacBook-Pro.local>
2014-11-15 16:12:39 +1300

    login

--- scripts/helper.js
+++ scripts/helper.js
@@ -139,7 +139,15 @@ function getCurrentDateTime() {
     return dateTime;
 }
 
-
+function parseGet(val) {
+    var result = null, tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
 
 
 
