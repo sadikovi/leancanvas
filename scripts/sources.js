db82a2f9059737f1d0351a757a8ad40b7326d61b
sadikovi <sadikovi@Ivans-MacBook-Pro.local>
2014-11-15 15:32:09 +1300

    sources.js fix

--- scripts/sources.js
+++ scripts/sources.js
@@ -1,13 +1,13 @@
 // sources for all the images and other things that have to be linked inside scripts
 var Source = {
-    IMG_ADD_NOTE = "/resources/images/note_add.png",
-    IMG_SPINNER = "/resources/images/spinner.gif",
-    IMG_OK_SMALL = "/resources/images/small_ok.png",
-    IMG_CANCEL_SMALL = "/resources/images/small_cancel.png",
-    IMG_NOTE_EDIT = "/resources/images/note_delete.png",
-    IMG_NOTE_DELETE = "/resources/images/note_edit.png",
-    IMG_EDITOR_OK = "/resources/images/editor_submit.png",
-    IMG_EDITOR_CANCEL = "/resources/images/editor_close.png",
-    IMG_GITHUB_LOAD = "/resources/images/github_load.png",
-    IMG_GITHUB_SAVE = "/resources/images/github_save.png"
+    IMG_ADD_NOTE : "/resources/images/note_add.png",
+    IMG_SPINNER : "/resources/images/spinner.gif",
+    IMG_OK_SMALL : "/resources/images/small_ok.png",
+    IMG_CANCEL_SMALL : "/resources/images/small_cancel.png",
+    IMG_NOTE_EDIT : "/resources/images/note_delete.png",
+    IMG_NOTE_DELETE : "/resources/images/note_edit.png",
+    IMG_EDITOR_OK : "/resources/images/editor_submit.png",
+    IMG_EDITOR_CANCEL : "/resources/images/editor_close.png",
+    IMG_GITHUB_LOAD : "/resources/images/github_load.png",
+    IMG_GITHUB_SAVE : "/resources/images/github_save.png"
 };
